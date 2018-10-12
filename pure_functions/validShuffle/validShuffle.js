30c7b49315c60e2e8a37f7c5ef604d6f6650dd2d
Dan Kreiger <dan@dankreiger.com>
2018-10-12 20:12:12 +0200

    Use O(n) function

--- pure_functions/validShuffle/validShuffle.js
+++ pure_functions/validShuffle/validShuffle.js
@@ -1,13 +1,12 @@
-
 const validShuffleO_N = require('./../../drafts/validShuffleO_N');
 const validShuffleO_N_another = require('./../../drafts/validShuffleO_N_another');
 const validShuffleO_N_Log_N = require('./../../drafts/validShuffleO_N_Log_N');
 
 
-function validShuffle(a, b){
-  return validShuffleO_N_Log_N(a, b);
+function validShuffle(a, b) {
+  // return validShuffleO_N_Log_N(a, b);
   // return validShuffleO_N(a, b); 
-  // return validShuffleO_N_another(a, b); 
+  return validShuffleO_N_another(a, b);
 }
 
 
